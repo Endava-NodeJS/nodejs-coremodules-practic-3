26d5a1356705b085763191fd116df5e6d715be16
dgluscenco <denis.gluscenco@mastercard.com>
2021-04-08 15:04:44 +0300

    smth

--- index.js
+++ index.js
@@ -1,4 +1,34 @@
+const http = require('http')
 const DB = require('./db');
 const db = new DB('./notes.json');
 
-console.log(db.get());
+const host = 'localhost';
+const port = 8080;
+
+const requestListener = ({method, url}, res) => {
+    const [, smth, id] = url.split('/')
+    switch (smth) {
+        case "notes":
+            if (method === 'GET') {
+                const data = id ? db.get(id): db.get()
+                res.setHeader("Content-Type", "application/json");
+                res.writeHead(200);
+                res.end(JSON.stringify(data));
+            }
+            break
+        default:
+            res.writeHead(200)
+            res.end('RAQ')
+            break;
+    }
+}
+
+const server = http.createServer(requestListener);
+
+server.listen(port, host, (error) => {
+    if (error) {
+        console.log('RAQ', error)
+    } else {
+        console.log(`Is happening something great on port ${port}...`)
+    }
+})
