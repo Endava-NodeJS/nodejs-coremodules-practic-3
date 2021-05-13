b06daaac4507a812fba48d1c2aa2ef7f07881563
Vladislav Mardari <Vladislav.Mardari@endava.com>
2021-05-13 15:11:23 +0300

    signin added

--- practica-4/controllers/auth/index.js
+++ practica-4/controllers/auth/index.js
@@ -1,5 +1,7 @@
 const db = require("../../models");
 const bcrypt = require("bcrypt");
+const jwt = require('jsonwebtoken');
+const { secret } = require("../../config/auth.config");
 
 module.exports = (app) => {
   app.use((req, res, next) => {
@@ -38,6 +40,45 @@ module.exports = (app) => {
       await Users.create({ username, password: hashedPassword });
 
       res.status(201).json({ message: "Users successfully created" });
+    } catch (e) {
+      console.error(e);
+      res.status(500);
+      res.end("Internal server error.");
+    }
+  });
+
+  app.post("/signin", async (req, res) => {
+    const { username, password } = req.body;
+    const Users = db.users;
+    let user;
+
+    if (!(password || username)) {
+      res.status(400);
+      res.end("Bad request. Password and username fields are required.");
+      return;
+    }
+
+    try {
+      user = await Users.findOne({ where: { username } });
+
+      if (!user) {
+        res.status(404);
+        res.end(`User not found`);
+        return;
+      }
+    } catch (e) {
+      console.error(e.message);
+    }
+
+    try {
+      const isPasswordsMatch = bcrypt.compareSync(password, user.password);
+
+      if(isPasswordsMatch) {
+        const token = jwt.sign({username}, secret, {expiresIn: 86400});
+        res.status(200).json({ accesToken: token });
+      } else {
+        res.status(401).end('Unauthorized!');
+      }
     } catch (e) {
       console.error(e);
       res.status(500);
